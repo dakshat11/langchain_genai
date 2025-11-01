66854ad8d706b2405c68511481ad31bc4210e0c6
Dakshat Pawale <dakshatpawale8@gmail.com>
2025-11-01 19:27:19 +0530

    pdf_rag

--- MODELS/2.ChatModels/5_hcat_hf_local.py
+++ MODELS/2.ChatModels/5_hcat_hf_local.py
@@ -1,5 +1,4 @@
-# hugging face local model
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
-
-import os
-
+from langchain_huggingface import HuggingFaceChatModel
+model = HuggingFaceChatModel(model_name="hcat-hf-local", model_kwargs={"device": "cpu"})
+response = model.predict_messages([{"role": "user", "content": "Hello world!"}])
+print(response.content) 
